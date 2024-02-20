881b8da13ca13dd2a7a594f07c6ec13868c4b49e
Adi Arta Wibawa <75978314+adiartawibawa@users.noreply.github.com>
2024-02-20 15:51:11 +0800

    update category

--- resources/views/livewire/pages/articles/all-article.blade.php
+++ resources/views/livewire/pages/articles/all-article.blade.php
@@ -1,15 +1,15 @@
 <div>
     <div class="container px-6 py-8 mx-auto lg:py-16">
+        @if ($categoryType === 'topic')
+            <p class="text-md font-bold"> Topic : <span class="text-indigo-500 text-sm">
+                    {{ $category }}</span></p>
+        @elseif ($categoryType === 'tags')
+            <p class="text-md font-bold"> Tags :<span class="text-indigo-500 text-sm"> {{ $category }}</span>
+
+            </p>
+        @endif
         <div class="max-w-screen-xl py-6 mt-4 -mx-2 grid grid-cols-1 gap-8 xl:gap-12 md:grid-cols-2 xl:grid-cols-3">
             @foreach ($articles as $article)
-                @if ($categoryType === 'topic')
-                    <p>Topic: {{ $category }}</p>
-                @elseif ($categoryType === 'tags')
-                    <p>Tags:{{ $category }}
-
-                    </p>
-                @endif
-
                 <div class="rounded overflow-hidden flex flex-col max-w-md mx-auto">
                     <a href="{{ route('article.single', $article->slug) }}">
                         <img class="w-full" src="{{ $article->featured_image_url }}"
